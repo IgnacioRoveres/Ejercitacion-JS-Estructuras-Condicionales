0e9c3851abfa1fb3165e4ebd262b8864ae28dd9e
ignacioroveres <ignaciovroveres10@gmail.com>
2024-06-30 21:19:34 -0300

    ejercicio 1 hecho

--- 1_NumeroPositivoNegativo.js
+++ 1_NumeroPositivoNegativo.js
@@ -1,1 +1,18 @@
 // 1. Leer una variable N de tipo entero e informar si el numero es positivo, negativo o cero y también si es par, impar o cero. El número cero no es ni positivo ni negativo ni par ni impar.
+
+const numero = -11;
+
+if (numero > 0) {
+    console.log("El número es positivo");
+} else if (numero < 0) {
+    console.log("El número es negativo");
+} else {
+    console.log("El número es cero");
+}
+
+if (numero % 2 === 0){
+    console.log("el numero es par")
+}
+else {
+    console.log("el numero es impar")
+}
